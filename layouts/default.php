7683d41ca73c7de52428fb9d57eea084f25df8a3
Niki <39004156+zzniki@users.noreply.github.com>
2024-05-21 10:05:53 +0200

    Contact page

--- layouts/default.php
+++ layouts/default.php
@@ -1,8 +1,13 @@
-<template>
-
+<head>
     <script src="/assets/dep/svg-loader.min.js"></script>
     <script src="/assets/dep/gsap.min.js"></script>
     <script src="/assets/dep/ScrollTrigger.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/main.css">
+</head>
+
+<template>
 
     <Loader/>
 
--- pages/contact.php
+++ pages/contact.php
@@ -1,35 +1,100 @@
 <template>
-    <div class="page">
-        <h1>I know this contact page sucks</h1>
-        <span>but i'm working on it</span>
 
-        <h2>Discord: @zzniki</h2>
-        <h2>Email: zznikiofficial@gmail.com</h2>
+    <div class="text">
+        <h1 class="salute">Hello!</h1>
+        <h2 style="margin-top: .1rem; margin-bottom: .5rem;">My real name is <span class="name">Lluna</span><span class="phonetic">/ˈʎu.nə/</span></h2>
+        <span>(niki is just a nickname)</span>
+    </div>
+
+    <div class="middletext">
+        <p>I am an 18 year old geek from Spain (Barcelona)</p>
+        <p>Who is obsessed with: </p>
+        <ul>
+            <li>Programming</li>
+            <li>Making music</li>
+        </ul>
+    </div>
+
+    <div class="icons">
+        <a href="">
+            <span>@zzniki</span>
+            <i class="bi bi-discord"></i>
+        </a>
+
+        <a href="https://open.spotify.com/intl-es/artist/68nay5v0gAzXEo2XJbdXpf?si=eKRp5J9QROmW1Yi-v3SgLQ" target="_blank">
+            <span>zzniki</span>
+            <i class="bi bi-spotify"></i>
+        </a>
+        
+        <a href="mailto:zznikiofficial@gmail.com">
+            <span>zznikiofficial@gmail.com</span>
+            <i class="bi bi-envelope-at-fill"></i>
+        </a>
     </div>
 </template>
 
 <style scoped="true">
 
-.page {
+.text {
+    margin-top: 3rem;
+    margin-left: 1rem;
+}
+
+.salute {
+    margin: 0px;
+    font-size: 80px;
+}
+
+.name {
+    background-color: white;
+    color: var(--color-background);
+
+    padding-block: .125em;
+    padding-inline: .25em;
+}
+
+.phonetic {
+    filter: opacity(50%);
+    margin-left: .5rem;
+}
+
+.middletext {
+    margin-inline: auto;
+    border: 2px solid var(--color-text);
+    width: fit-content;
+    padding: 1rem;
+}
+
+.icons {
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+.icons > a {
     display: flex;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
-
-    height: 100svh;
-    width: 100%;
+    
+    cursor: pointer;
+    text-decoration: none;
+    gap: 1rem;
 }
 
-h1 {
-    margin-bottom: .25rem;
+.icons > a > i {
+    font-size: 38px;
 }
 
-span {
-    margin-bottom: 2rem;
+.icons > a > span {
+    font-family: var(--font-console);
 }
 
-h2 {
-    margin-block: .25rem;
+.icons > a:hover > span {
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: var(--color-text);
+    color: transparent;
 }
 
 </style>
